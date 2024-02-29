2a8812e0bd035d23a5437d4e206720e2ec4c04a9
THIWAKORN <thiwakorn.sri@bualuang.co.th>
2024-02-29 11:51:45 +0700

    Complete App

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        // Set Alpha of Button to 0.5
+        sender.alpha = 0.5
+        
+        playSound(resourceName: sender.currentTitle!)
+        
+        // Set Alpha of Button to 1 (Reset after 0.2 seconds)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
     
-
+    func playSound(resourceName: String) {
+        let url = Bundle.main.url(forResource: resourceName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
